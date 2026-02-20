b4b68315592b99444af4810d3e88a19499d921f3
Mastewal <mastewalgshiferaw@gmail.com>
2026-02-20 10:12:29 +0300

    feat: Define Task model and register in admin

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICE = [
+        ('L', 'Low'),
+        ('M', 'Middle'),
+        ('H', 'High')
+
+    ]
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(blank=True, null=True)
+    priority=models.CharField(max_length=1, choices=PRIORITY_CHOICE, blank=True, null=True, help_text="Task Priority (Low, Medium, High)")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering=['created_at']
+
+
