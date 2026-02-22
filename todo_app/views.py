d0be6cce11c3f4350e192be14802b2a579ae8873
Mastewal <mastewalgshiferaw@gmail.com>
2026-02-22 19:35:32 +0300

    added filtering, searching, ordering to task API

--- todo_app/views.py
+++ todo_app/views.py
@@ -7,16 +7,18 @@ class TaskListCreate(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    filterset_fields = ['completed', 'priority', 'due_date']
-
-    search_fields = ['title', 'description']
-
-    ordering_fields = ['created_at', 'due_date', 'title', 'priority']
+   
 
 #View to retrieve, update, or delete a single task
 class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
+    filterset_fields = ['completed', 'priority', 'due_date']
+
+    search_fields = ['title', 'description']
+
+    ordering_fields = ['created_at', 'due_date', 'title', 'priority']
 
 # View to retrieve, update, or delete a single task
 class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
