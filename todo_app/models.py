6f0d8e8d64eca3cef00caeb896f0aba7eca6bed5
Mastewal <mastewalgshiferaw@gmail.com>
2026-02-28 23:51:45 +0300

    added permissions.py

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,21 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User # Import Django's built-in User model
 
 class Task(models.Model):
-    PRIORITY_CHOICE = [
+    PRIORITY_CHOICES = [
         ('L', 'Low'),
-        ('M', 'Middle'),
-        ('H', 'High')
-
+        ('M', 'Medium'),
+        ('H', 'High'),
     ]
+
+    owner = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE, null=True, blank=True) # New field!
     title = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateField(blank=True, null=True)
-    priority=models.CharField(max_length=1, choices=PRIORITY_CHOICE, blank=True, null=True, help_text="Task Priority (Low, Medium, High)")
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        blank=True,
+        null=True,
+        help_text="Task priority (Low, Medium, High)"
+    )
 
     def __str__(self):
         return self.title
-    
-    class Meta:
-        ordering=['created_at']
 
+    class Meta:
+        ordering = ['created_at']
