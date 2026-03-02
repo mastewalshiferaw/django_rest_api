82b163682e83a8af835ad5e400e9831dd2143066
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-02 23:52:08 +0300

    replace TaskListCreate and TaskRetrieveUpdateDestroy with a single ViewSet

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,25 +1,21 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
+from .permissions import IsOwnerOrReadOnly
 
-#a view to list all tasks and create new tasks
-class TaskListCreate(generics.ListCreateAPIView):
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-   
-
-#View to retrieve, update, or delete a single task
-class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    
-    filterset_fields = ['completed', 'priority', 'due_date']
-
+    serializer_class=TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filterset_fields = ['completed', 'priority', 'due_date', 'owner']
     search_fields = ['title', 'description']
-
     ordering_fields = ['created_at', 'due_date', 'title', 'priority']
 
+     def perform_create(self, serializer):
+        """Save the owner when a new task is created."""
+        serializer.save(owner=self.request.user)
 # View to retrieve, update, or delete a single task
 class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
