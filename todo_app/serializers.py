350f952074f3a540a66e6516831cd7ee1f5fea17
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-04 21:52:54 +0300

    feat: Nested UserSerializer for owner field in TaskSerializer

--- todo_app/serializers.py
+++ todo_app/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =User
+        fields = ('id', 'username', 'email')
+
 
 class TaskSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    #makes owner field nested and read only
     class Meta:
+        
         model = Task
         fields = '__all__'
