d2ee01cf6f20d7df1237cfa0e8943072640d7f65
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-08 23:56:07 +0300

    search field in taskview edited

--- todo_app/views.py
+++ todo_app/views.py
@@ -5,17 +5,32 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 
+from Response
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class=TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filterset_fields = ['completed', 'priority', 'due_date', 'owner']
-    search_fields = ['title', 'description']
+    search_fields = ['title']
     ordering_fields = ['created_at', 'due_date', 'title', 'priority']
 
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            if user.is_superuser:
+                return Task.objects.all()
+            return Task.objects.filter(owner=user)
+        return Task.objects.none()
+    
     def perform_create(self, serializer):
         """Save the owner when a new task is created."""
         serializer.save(owner=self.request.user)
+
+
+    
+
 # View to retrieve, update, or delete a single task
 class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
