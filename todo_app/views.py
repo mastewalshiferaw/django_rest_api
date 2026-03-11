5e6f328764396bd279fda965af5b843dc4023b39
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-11 23:52:17 +0300

    added new method to taskviewset (mark_completed)

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,12 +4,20 @@ from .serializers import TaskSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
-
+from rest_framework.decorators import action
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class=TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    
+    @action(detail=True, methods=['post'])
+    def mark_completed(self, request, pk=None):
+        task = self.get_object()   
 
+        task.completed = True      
+        task.save()                
 
+        serializer = self.get_serializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
