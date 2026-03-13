3f1b5420d29d9d3e78db0c244e725a4e71f237dc
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-13 14:10:13 +0300

    updated task view to handle user association

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,23 +1,26 @@
-from rest_framework import generics
-from .models import Task
-from .serializers import TaskSerializer, UserSerializer
-from rest_framework import viewsets
-from rest_framework import permissions
-from .permissions import IsOwnerOrReadOnly
+from rest_framework import generics, viewsets, permissions, status
+from rest_framework.response import Response
 from rest_framework.decorators import action
+from .models import Task
+from .serializers import TaskSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class=TaskSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        # Only return tasks belonging to the logged-in user
+        return Task.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically set the owner to the current user
+        serializer.save(owner=self.request.user)
+
     @action(detail=True, methods=['post'])
     def mark_completed(self, request, pk=None):
         task = self.get_object()   
-
         task.completed = True      
         task.save()                
-
         serializer = self.get_serializer(task)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
