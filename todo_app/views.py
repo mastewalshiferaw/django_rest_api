8314ea1c4813b0796986adea3c2dad977bb04f77
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-18 23:18:10 +0300

    added pagination and search capabilities

--- todo_app/views.py
+++ todo_app/views.py
@@ -5,7 +5,7 @@ from .models import Task
 from .serializers import TaskSerializer
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -13,11 +13,12 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
 
-    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filterset_fields = ['priority', 'completed']
     ordering_fields = ['created_at', 'due_date', 'priority']
     ordering = ['-created_at'] #Default: newest first
 
+    search_filter = ['title', 'description']
     def get_queryset(self):
         # Only return tasks belonging to the logged-in user
         return Task.objects.filter(owner=self.request.user)
