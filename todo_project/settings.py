0a06bd9a51ad0e70565d4e3a3735c8dc478b0f25
Mastewal <mastewalgshiferaw@gmail.com>
2026-03-26 23:18:50 +0300

    implement frontend token-based fetch flow

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -137,7 +137,10 @@ REST_FRAMEWORK = {''
     'PAGE_SIZE': 10
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", # Common for React
     "http://127.0.0.1:5500", # Common for VS Code Live Server
+    "null",
 ]
